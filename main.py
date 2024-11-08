1d3b3e35b4b4ab506c12fff86f67ccb20f2e76f9
vedanshipanchal <vedanshipanchal@gmail.com>
2024-11-08 16:14:02 +0000

    Current date and time

---
+++ main.py
@@ -0,0 +1,12 @@
+#import modules
+from datetime import date, time, datetime
+
+today = date.today()
+now = datetime.now()
+
+print("Today is:",today)
+print("Date and time is-",now)
+
+print("Month is",today.month)
+print("year is:",today.year)
+print("day is:",today.day)
